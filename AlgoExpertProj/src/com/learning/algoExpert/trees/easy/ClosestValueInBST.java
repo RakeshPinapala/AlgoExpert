af026534d4741dc912f6d1a7c03ca3410c025063
RakeshPinapala <53266223+RakeshPinapala@users.noreply.github.com>
2022-03-13 10:55:26 +0530

    Bst_closest

--- AlgoExpertProj/src/com/learning/algoExpert/trees/easy/ClosestValueInBST.java
+++ AlgoExpertProj/src/com/learning/algoExpert/trees/easy/ClosestValueInBST.java
@@ -5,5 +5,35 @@ public class ClosestValueInBST {
 	public static void main(String[] args) {
 
 	}
+	
+	static class BST {
+	    public int value;
+	    public BST left;
+	    public BST right;
+
+	    public BST(int value) {
+	      this.value = value;
+	    }
+	}
+	    
+    public static int findClosestValueInBst(BST tree, int target) {
+    	return traverseInOrder(tree,target,Math.abs(target-tree.value),tree);
+      }
+    
+    private static int traverseInOrder(BST tree, int target,int minDiff, BST minNode) {
+    	int currDiff = minDiff;
+    	if(Math.abs(target-tree.value) < minDiff) {
+    		currDiff = Math.abs(target-tree.value);
+    		minNode = tree;
+    	}
+    	if(tree.value == target)
+    		return target;
+    	else if(tree.right != null && tree.value < target) {
+    		return traverseInOrder(tree.right,target,currDiff,minNode);
+    	} else if(tree.left != null && tree.value > target){
+    		return traverseInOrder(tree.left,target,currDiff,minNode);
+    	}
+    	return minNode.value;
+    }
 
 }
